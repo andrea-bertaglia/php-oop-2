d68dd01664a66357b347e50d73bd7030ed14de34
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-03 17:36:55 +0200

    feat: definition class category

--- models/category.php
+++ models/category.php
@@ -1,1 +1,22 @@
 <?php
+
+class Category
+{
+    public string $category;
+
+    public function __construct($category)
+    {
+        if ($category !== 'cane' && $category !== 'gatto') {
+            throw new Exception("Le categorie possibili sono 'cane' o 'gatto'");
+        }
+        $this->category = $category;
+    }
+
+    /**
+     * Get the value of category
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
